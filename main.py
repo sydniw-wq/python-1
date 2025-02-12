8d12e38085f5e545ca71d2776a381720f8984cf8
sydni wright <sydniw@udel.edu>
2025-02-12 17:20:38 -0500

    working on convert to ascii function

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from PIL import IMAGE
 gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 gscale2 = " .:-=+*#%@"
 
+
+# MUST COMPUTE THE AVERAGE BRIGHTNESS FOR EACH TILE
 # now will define average function which takes in image (already in grayscale)
 # and coomputes the average pixel value of the grayscale intensities
 def averageL(image):
@@ -28,9 +30,48 @@ def averageL(image):
 
 
 # MUST SPLIT UP IMAGE INTO AN M * N GRID 
-
-# MUST COMPUTE THE AVERAGE BRIGHTNESS FOR EACH TILE
-
 # MUST GENERATE AN ASCII CHARACTER FOR EACH TILE DEPENDING ON BRIGHTNESS
+def convertToAscii(fileName, columns, scale, moreLevels):
+    #given an image and dimensions, return a M * N grid
+
+    #global grayscale variables
+    global gscale1, gscale2
+
+    #open image and convert to grayscale
+    image = IMAGE.open(fileName).convert('L')
+
+    #store dimensions
+    width, height = image.size[0], image.size[1]
+    print("input image dimensions: " + width + " by "+ height)
+
+    #compute dimensions of individual tiles
+    tileWidth = width/columns
+    tileHeight = height/scale
+    print("tile dimensions: "+ tileWidth+ " by "+tileHeight)
+
+    #compute number of rows
+    rows = int(height/tileHeight)
+    print("columns: "+columns+", rows: "+rows)
+
+    #verify image is not too small for specified columns
+    if columns > width or rows > height:
+        print("image too small")
+        exit(0)
+    
+    #define list for all ascii characters
+    asciiImg = []
+
+    #generate list of dimensions
+    for i in range(rows):
+        #y1 = starting y-cord of current row, y2=ending ending y-cord of current row
+        y1 = int(i*tileHeight)
+        y2 = int((j+1)*tileHeight)
+
+        #ensure last row extends to the full height of the grid
+        if i == rows-1:
+            y2 = height
+        
+        #append empty string for each row
+        asciiImg.append("")
 
 # MUST OPEN A NEW TEXT FILE TO OUTPUT TO - DISPLAY FULL IMAGE
